a27904e94757cc13ac71c153373c7be912a28ea3
azquispe <azquispe@bg.com.bo>
2022-12-10 00:02:49 -0400

    se ha optimizado push

--- lib/avisos/services/push_noitfications_service.dart
+++ lib/avisos/services/push_noitfications_service.dart
@@ -4,6 +4,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 
 
+
 // SHA1: 1D:4F:38:94:62:06:9F:C6:75:A7:73:BD:E4:B0:DA:27:80:B1:9D:F0
 // P8 - KeyID: VYZH37GGZ9
 
@@ -34,23 +35,27 @@ class PushNotificationService {
 
 
 
+
+
   static Future initializeApp() async {
 
     // Push Notifications
     await Firebase.initializeApp();
     await requestPermission();
 
-    token = await FirebaseMessaging.instance.getToken();
-    print('Token: $token');
+    /*token = await FirebaseMessaging.instance.getToken();
+    print('Token: $token');*/
 
     // Handlers
+
     FirebaseMessaging.onBackgroundMessage( _backgroundHandler );
     FirebaseMessaging.onMessage.listen( _onMessageHandler );
     FirebaseMessaging.onMessageOpenedApp.listen( _onMessageOpenApp );
 
     // Local Notifications
-
     messaging.subscribeToTopic('anuncios_ganaseguros_2022_dev'); // para recibir push
+
+
 
   }
 
